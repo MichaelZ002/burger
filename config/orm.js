5e4acf406cdeb139906c9cfc15afe20c77edb818
Michael Zaslavsky <mikezasl@gmail.com>
2020-08-08 21:55:43 -0500

    added the mdel and controller. fixed some bugs

--- config/orm.js
+++ config/orm.js
@@ -25,3 +25,4 @@ const orm = {
         });
     }
 };
+module.exports = orm
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const express = require("expr")
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const exphbs = require("express-handlebars");
+const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+const routes = require("./controllers/burgers_controller");
+app.use(routes);
+app.listen(PORT, () => {console.log("Server listening on: http://localhost:" + PORT)});
